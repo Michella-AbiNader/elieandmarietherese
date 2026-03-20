9bdbebf601d104bd92ed4f939521a626b9366bb2
Michella Abi Nader <michella.abi.nader@gmail.com>
2026-03-20 15:15:30 +0200

    Move Firebase config to .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClXkeRM-cKgFVIv5AI6DOzKihRhgEUwxU",
-  authDomain: "elie-and-marietherese.firebaseapp.com",
-  projectId: "elie-and-marietherese",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "elie-and-marietherese.firebasestorage.app",
   messagingSenderId: "152238688917",
   appId: "1:152238688917:web:b966e1f5d9fa1cf8ada99c"
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
