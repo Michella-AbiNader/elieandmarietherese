e98b4590b732759702e753b46db4e7f271da8651
Michella Abi Nader <michella.abi.nader@gmail.com>
2026-03-28 13:15:43 +0200

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import InvitationApp from "./Components/Invitation";
+import AdminDashboard from "./Components/AdminDashboard";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      {/* Invitation */}
+      <Route path="/" element={<InvitationApp />} />
+
+      {/* Dashboard */}
+      <Route path="/admin" element={<AdminDashboard />} />
+    </Routes>
   </BrowserRouter>
 );
 // import { StrictMode } from 'react'
